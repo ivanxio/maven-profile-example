81d0015ea5b95e91720568ee1df1ff3f7a16ebb5
Iván García Martínez <ivan.garcia@amk-technologies.com>
2022-04-13 16:22:17 -0500

    The profile seccion was added in the pom.xml file
    The filters and resources section were added in the build section in the pom.xml file.
    
    An example code was added to tests the maven profile configuration.

--- src/main/java/com/igm/app01/App.java
+++ src/main/java/com/igm/app01/App.java
@@ -1,13 +1,34 @@
 package com.igm.app01;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/** The example project using the MAVEN profiles. */
+public class App {
+
+    /**
+     * Main method.
+     *
+     * @param args The argument from command line.
+     */
+    public static void main(final String[] args) {
+        final App app = new App();
+        final Properties properties = app.loadProperties();
+        if (properties != null) {
+            properties.forEach((v, k) -> System.out.println(String.format("%s: %s", v, k)));
+        } else {
+            System.err.println("Faild to load properties file: configuration.properties");            
+        }
+    }
+
+    private Properties loadProperties() {
+        final Properties properties = new Properties();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("configuration.properties")) {
+            properties.load(inputStream);
+        } catch (final IOException e) {            
+            return null;
+        }
+        return properties;
     }
 }
